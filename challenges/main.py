04c277a62946fa03641fbf5b6d749a37c3188f69
bright-webb <60761289+bright-webb@users.noreply.github.com>
2026-09-18 09:15:16 +0100

    Complete: Fix the Discount Applier

--- challenges/main.py
+++ challenges/main.py
@@ -1,27 +1,2 @@
-import sys
-
-def is_valid_isbn_10(isbn):
-    if len(isbn) != 10:
-        return False
-
-    total_sum = 0
-    for i in range(10):
-        char = isbn[i]
-        if i < 9:
-            if not char.isdigit():
-                return False
-            digit = int(char)
-        else:  # Last character
-            if char.isdigit():
-                digit = int(char)
-            elif char.upper() == 'X':
-                digit = 10
-            else:
-                return False
-        
-        total_sum += (10 - i) * digit
-
-    return total_sum % 11 == 0
-
-isbn_string = sys.stdin.readline().strip()
-print(is_valid_isbn_10(isbn_string))
+def apply_discount(price, discount_percent=10):
+    return round(price - (price * discount_percent / 100), 2)
